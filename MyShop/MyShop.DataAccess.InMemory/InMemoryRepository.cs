e8f0ebb7f8f7e29767734eaf15683f37e790550e
FOLIAGOS Development Team <developer@foliagos.com>
2019-08-24 20:25:26 +0100

    Implemented generic Repository

---
+++ MyShop/MyShop.DataAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,86 @@
+﻿using MyShop.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DataAccess.InMemory
+{
+    public class InMemoryRepository<T> where T : BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;  // We will create a list of the object that is passed in via the <T> placeholder
+        string className;   // This will hold the name of the cache we are working with e.g. 'Product' or 'ProductCategory'
+
+        public InMemoryRepository() // This is the Constructor for this class (it has the same name as the class)
+        {
+            // When we call this generic class we pass in the object we want to perform the cache functions on e.g. 'Product' or 'ProductCategory'.
+            // We need to extract the name of the object, so we use a technique called "REFLECTION" to do this.
+            // The typeof() method exposes the definition of the object so we can then extract the name of it and store it in 'className'.
+            className = typeof(T).Name;
+            items = cache[className] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+
+        public void Commit()
+        {
+            cache[className] = items;
+        }
+
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(className + " Not Found");
+            }
+        }
+
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(className + " Not Found");
+            }
+        }
+
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            T tToDelete = items.Find(i => i.Id == Id);
+
+            if (tToDelete != null)
+            {
+                items.Remove(tToDelete);
+            }
+            else
+            {
+                throw new Exception(className + " Not Found");
+            }
+        }
+    }
+}
